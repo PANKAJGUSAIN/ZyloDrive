6d8f0c4d15236effbf23c57153eca05745abf1c2
Pankaj Gusain <gusainpankaj1999@gmail.com>
2025-01-08 13:59:42 +0530

    Location search bar created

--- Frontend/src/components/LocationSearchPanel/LocationSearchPanel.jsx
+++ Frontend/src/components/LocationSearchPanel/LocationSearchPanel.jsx
@@ -1,7 +1,19 @@
-const LocationSearchPanel = () =>{
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import styles from './LocationSearchPanel.module.scss'
+
+const LocationSearchPanel = () => {
     return (
         <>
-            LocationSearchPanel
+            <h4 style={{ padding:"10px 10px"}}>LocationSearchPanel</h4>
+            <div className={styles.LocationSearchPanelWrapper}>
+                <div className={styles.LocationSearchItem}>
+                    <div>
+                        <FontAwesomeIcon icon={faLocationDot} style={{color:"var(--text-color)"}} />
+                    </div>
+                    <p>24B , Near Kapoor's cafe, Sheryians Coding School , Bhopal</p>
+                </div>
+            </div>
         </>
     )
 }
