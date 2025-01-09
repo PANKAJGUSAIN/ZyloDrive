32378a1392cfda3d4b09b37dd68dbfad1c1e8a1c
Pankaj Gusain <gusainpankaj1999@gmail.com>
2025-01-09 21:58:19 +0530

    fix theme selector

--- Frontend/src/Context/ThemeContext.jsx
+++ Frontend/src/Context/ThemeContext.jsx
@@ -3,7 +3,8 @@ import { createContext, useState, useEffect } from 'react';
 const ThemeContext = createContext();
 
 const ThemeProvider = ({ children }) => {
-    const AvaliableThemes = ["Light , Dark  , HightContrast"];
+    const AvaliableThemes = ["Light", "Dark"];
+    const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
 
     // Get user's preferred theme from localStorage or default to the browser's preference
     const getInitialTheme = () => {
@@ -13,7 +14,7 @@ const ThemeProvider = ({ children }) => {
         }
 
         // Check for the user's system preference
-        const userPrefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+        const userPrefersDark = mediaQuery.matches;
 
         if (userPrefersDark) {
             return 'Dark'; // If the system prefers dark mode
@@ -32,8 +33,8 @@ const ThemeProvider = ({ children }) => {
         document.body.setAttribute('data-theme', currentTheme); // Apply the data-theme attribute to the body
     }, [currentTheme]);
 
-     // Listen for changes to localStorage in other tabs/windows
-     useEffect(() => {
+    // Listen for changes to localStorage in other tabs/windows
+    useEffect(() => {
         const handleStorageChange = () => {
             const themeFromStorage = localStorage.getItem('theme');
             if (themeFromStorage && themeFromStorage !== currentTheme) {
@@ -41,14 +42,26 @@ const ThemeProvider = ({ children }) => {
             }
         };
 
+        // Function to update the theme based on user preference
+        const handleThemeChange = (e) => {
+            setCurrentTheme(e.matches ? 'Dark' : 'Light');
+        };
+
         // Set up the event listener
         window.addEventListener('storage', handleStorageChange);
+
+
+        // Add event listener to listen for changes in theme
+        mediaQuery.addEventListener('change', handleThemeChange);
+
+
 
         // Cleanup the listener on component unmount
         return () => {
             window.removeEventListener('storage', handleStorageChange);
+            mediaQuery.removeEventListener('change', handleThemeChange);
         };
-    }, [currentTheme]);
+    }, []);
 
 
 
