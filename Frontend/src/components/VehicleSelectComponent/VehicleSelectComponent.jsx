bf1bb837f4154287c24f30da4557423eddfb5ad6
Pankaj Gusain <gusainpankaj1999@gmail.com>
2025-01-10 17:14:35 +0530

    minor fix

--- Frontend/src/components/VehicleSelectComponent/VehicleSelectComponent.jsx
+++ Frontend/src/components/VehicleSelectComponent/VehicleSelectComponent.jsx
@@ -91,57 +91,6 @@ const VehicleSelectComponent = forwardRef(({ handleClick }, ref) => {
                             <h5 className={styles.vehiclePrice}>$ 23.50</h5>
                         </div>
                     </div>
-                    <div className={styles.vehicleContainer}>
-                        <img loading="lazy" className={styles.vehicleImage} src={carimg} alt="Car" />
-                        <div>
-                            <div className={styles.vehicleDetails}>
-                                <h5>Uber Go</h5>
-                                <FontAwesomeIcon className={styles.vehicleSeatIcon} icon={faUserFriends} style={{ color: "var(--text-color)" }} />
-                                <p style={{ fontSize: "clamp(12px, calc(12px + 0.5vw), 16px)" }}>4</p>
-                            </div>
-                            <div className={styles.vehicleTime}>
-                                <div><p>2 mins away 15:03pm </p></div>
-                            </div>
-                            <div className={styles.vehicleStyle}><p>Affordable, Compact Rides</p></div>
-                        </div>
-                        <div>
-                            <h5 className={styles.vehiclePrice}>$ 83.50</h5>
-                        </div>
-                    </div>
-                    <div className={styles.vehicleContainer}>
-                        <img loading="lazy" className={styles.vehicleImage} src={autoimg} alt="Auto" />
-                        <div>
-                            <div className={styles.vehicleDetails}>
-                                <h5>Uber Auto</h5>
-                                <FontAwesomeIcon className={styles.vehicleSeatIcon} icon={faUserFriends} style={{ color: "var(--text-color)" }} />
-                                <p style={{ fontSize: "clamp(12px, calc(12px + 0.5vw), 16px)" }}>3</p>
-                            </div>
-                            <div className={styles.vehicleTime}>
-                                <div><p>2 mins away 15:03pm </p></div>
-                            </div>
-                            <div className={styles.vehicleStyle}><p>Affordable, Compact Rides</p></div>
-                        </div>
-                        <div>
-                            <h5 className={styles.vehiclePrice}>$ 43.50</h5>
-                        </div>
-                    </div>
-                    <div className={styles.vehicleContainer}>
-                        <img loading="lazy" className={styles.vehicleImage} src={bikeimg} alt="Bike" />
-                        <div>
-                            <div className={styles.vehicleDetails}>
-                                <h5>Uber Bike</h5>
-                                <FontAwesomeIcon className={styles.vehicleSeatIcon} icon={faUserFriends} style={{ color: "var(--text-color)" }} />
-                                <p style={{ fontSize: "clamp(12px, calc(12px + 0.5vw), 16px)" }}>2</p>
-                            </div>
-                            <div className={styles.vehicleTime}>
-                                <div><p>2 mins away 15:03pm </p></div>
-                            </div>
-                            <div className={styles.vehicleStyle}><p>Affordable, Compact Rides</p></div>
-                        </div>
-                        <div>
-                            <h5 className={styles.vehiclePrice}>$ 23.50</h5>
-                        </div>
-                    </div>
                 </div>
             }
 
