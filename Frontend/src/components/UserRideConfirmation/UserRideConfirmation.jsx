3e1468b0456399f75b47ce59ff03424b95f4dfc4
Pankaj Gusain <gusainpankaj1999@gmail.com>
2025-01-23 22:26:45 +0530

    integrated userRide creation to ui

--- Frontend/src/components/UserRideConfirmation/UserRideConfirmation.jsx
+++ Frontend/src/components/UserRideConfirmation/UserRideConfirmation.jsx
@@ -12,14 +12,15 @@ const UserRideConfirmation = forwardRef(({ }, ref) => {
     const location = useLocation();
     const navigate = useNavigate();
     const { state } = location;
-    const { vehicle: selectedVehicle, location: selectedLocation } = state || {};
+    const { vehicle: selectedVehicle, location:selectedLocation , fare } = state || {};
     const [userWrapperRef] = ref;
     const [loading, setLoading] = useState(true)
+    const [rideCreating , setRideCreating] = useState(false)
 
     useEffect(() => {
 
         console.log('location', state)
-        if (!selectedVehicle && !selectedLocation) {
+        if (!selectedVehicle && !selectedLocation?.pickup && !selectedLocation?.destination) {
             navigate("/home")
         }
         setLoading(false)
@@ -34,6 +35,43 @@ const UserRideConfirmation = forwardRef(({ }, ref) => {
         }
     }, [])
 
+
+    const handleRideCreation = async() => {
+        setRideCreating(true)
+        try{
+            const response = await fetch(`${process.env.REACT_APP_API_URL}/rides/create`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${sessionStorage.getItem('zylotoken')}`
+                },
+                body: JSON.stringify({
+                    vehicleType: selectedVehicle,
+                    pickup:selectedLocation.pickup,
+                    destination:selectedLocation.destination
+                })
+            })
+    
+            if (!response.ok) {
+                // Handle HTTP errors
+                const errorData = await response.json();
+                throw new Error(`Error ${response.status}: ${errorData.message || response.statusText}`);
+            } 
+            else{
+                const data = await response.json()
+                console.log(data)
+                navigate("/home/ridelookout", {state:{vehicle:selectedVehicle, location:selectedLocation , data}})
+            }
+        }
+        catch(error){
+            console.log(error)
+        }finally{
+            setRideCreating(false)
+        }
+        
+    }
+
+
     return (
         <>
             {loading ?
@@ -41,29 +79,29 @@ const UserRideConfirmation = forwardRef(({ }, ref) => {
                 <div className={styles.RideLookupComponentWrapper}>
                     <h3 style={{ padding: "10px 10px 10px 10px", textAlign: "center" }}>Confirm {selectedVehicle} Ride ? </h3>
                     <div style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
-                        {selectedVehicle=="Car" && <img loading="lazy" className={styles.RideLookupVehicleLoading} src={carimg} alt="Car" />}
-                        {selectedVehicle=="Auto" && <img loading="lazy" className={styles.RideLookupVehicleLoading} src={autoimg} alt="Auto" />}
-                        {selectedVehicle=="Bike" && <img loading="lazy" className={styles.RideLookupVehicleLoading} src={bikeimg} alt="Bike" />}
+                        {selectedVehicle=="car" && <img loading="lazy" className={styles.RideLookupVehicleLoading} src={carimg} alt="Car" />}
+                        {selectedVehicle=="auto" && <img loading="lazy" className={styles.RideLookupVehicleLoading} src={autoimg} alt="Auto" />}
+                        {selectedVehicle=="motorcycle" && <img loading="lazy" className={styles.RideLookupVehicleLoading} src={bikeimg} alt="Bike" />}
                     </div>
                     <div className={styles.RideLookupline}>
                     </div>
                     <div className={styles.ridelocationdetials}>
                         <FontAwesomeIcon icon={faLocationDot} style={{ color: "var(--text-color)" }} />
-                        <h5>{selectedLocation}</h5>
+                        <h5>{selectedLocation?.pickup}</h5>
                     </div>
                     <div className={styles.RideLookupline}>
                     </div>
                     <div className={styles.ridelocationdetials}>
                         <FontAwesomeIcon icon={faBox} style={{ color: "var(--text-color)" }} />
-                        <h5>{selectedLocation}</h5>
+                        <h5>{selectedLocation?.destination}</h5>
                     </div>
                     <div className={styles.RideLookupline}>
                     </div>
                     <div className={styles.ridelocationdetials}>
                         <FontAwesomeIcon icon={faCashRegister} style={{ color: "var(--text-color)" }} />
-                        <h5>$123.34</h5>
+                        <h5>{`\u20B9 ${fare}`}</h5>
                     </div>
-                    <Button type="submit" onClick={()=>{navigate('/home/ridelookout' , { state: {vehicle:selectedVehicle , location:selectedLocation} } )}}>Confirm Booking</Button>
+                    <Button type="submit" disabled={rideCreating} onClick={()=>{handleRideCreation()}}>{rideCreating ? "Creating Ride ..." :"Confirm Booking"}</Button>
                 </div>
             }
 
